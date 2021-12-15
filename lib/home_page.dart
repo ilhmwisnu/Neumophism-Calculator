20313da415346ce0f65757347c74f07ade1ec1c0
ilhmwisnu <ilhamwisnu42@gmail.com>
2021-12-16 01:02:02 +0700

    update keyboard

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -55,7 +56,7 @@ class HomePage extends StatelessWidget {
           child: Row(
             children: [
               Container(
-                color: Colors.blue,
+                // color: Colors.blue,
                 width: buttonWidth * 3 + 16 * 3,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -112,15 +113,23 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               Container(
-                color: Colors.red,
+                // color: Colors.red,
                 width: buttonWidth,
                 child: Column(
                   children: [
                     Button(),
                     SizedBox(height: 16),
-                    Expanded(child: Button()),
+                    Expanded(
+                        child: Button(
+                      isOuterShadow: false,
+                    )),
                     SizedBox(height: 16),
-                    Expanded(child: Button()),
+                    Expanded(
+                        child: Button(
+                      child: "=",
+                      color: Colorss.amberColor,
+                      isOuterShadow: false,
+                    )),
                   ],
                 ),
               )
@@ -133,19 +142,51 @@ class HomePage extends StatelessWidget {
 }
 
 class Button extends StatelessWidget {
+  Color? color = Colorss.textColor;
   final String child;
-  const Button({this.child = ' '});
+  final bool isOuterShadow;
+  Button({this.child = ' ', this.isOuterShadow = true, this.color});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50), color: Colorss.primaryColor),
+          borderRadius: BorderRadius.circular(50),
+          color: Colorss.primaryColor,
+          boxShadow: (isOuterShadow)
+              ? [
+                  BoxShadow(
+                    blurRadius: 12,
+                    offset: Offset(5, 5),
+                    color: Color(0xff000000).withOpacity(0.1),
+                  ),
+                  BoxShadow(
+                      blurRadius: 12,
+                      offset: Offset(-5, -5),
+                      color: Color(0xffffffff).withOpacity(0.66))
+                ]
+              : [],
+          gradient: (!isOuterShadow)
+              ? LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.black.withOpacity(0.7),
+                    Colors.black.withOpacity(0.05),
+                    Colors.black.withOpacity(0.01),
+                    Colors.black.withOpacity(0),
+                    Colors.white.withOpacity(0),
+                    Colors.white.withOpacity(0.1),
+                    Colors.white.withOpacity(0.3),
+                    Colors.white.withOpacity(0.7),
+                    Colors.white.withOpacity(1),
+                  ],
+                  transform: GradientRotation(-0.5))
+              : null),
       alignment: Alignment.center,
       width: (MediaQuery.of(context).size.width - 16 * 5) * (1 / 4),
       height: (MediaQuery.of(context).size.width - 16 * 5) * (1 / 4),
-      // color: Colors.amber,
-      child: Text(child),
+      child: Text(child, style: TextStyle(color: color, fontSize: 40)),
     );
   }
 }
